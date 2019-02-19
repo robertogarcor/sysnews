8c36dddb05d5f87b57d063676d13d0bfc51c29be
robertogarcor <groberto74@gmail.com>
2019-02-19 15:11:31 +0100

    create package

---
+++ Sysnews/src/main/java/com/roberto/jpa/sysnews/SysnewsApplication.java
@@ -0,0 +1,13 @@
+package com.roberto.jpa.sysnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysnewsApplication.class, args);
+	}
+
+}
---
+++ Sysnews/src/test/java/com/roberto/jpa/sysnews/SysnewsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.roberto.jpa.sysnews;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SysnewsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
